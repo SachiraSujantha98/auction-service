8bf11bbbed6d373d6d3dda25ba9b3d15f6c70935
SachiraSujantha98 <sachirasujanthamp@gmail.com>
2025-02-12 01:13:41 +0530

    set pic s3 url in dynamodb

--- src/handlers/uploadAuctionPicture.ts
+++ src/handlers/uploadAuctionPicture.ts
@@ -1,8 +1,31 @@
+import middy from "@middy/core";
+import httpErrorHandler from "@middy/http-error-handler";
+import createHttpError from "http-errors";
+import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
+import { getAuctionById } from "./getAuction";
+import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
+
 const uploadAuctionPicture = async (event) => {
+    const { id } = event.pathParameters;
+    const auction = await getAuctionById(id);
+    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+    const buffer = Buffer.from(base64, 'base64');
+
+    let updatedAuction;
+
+    try {
+        const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+        updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
+
+    } catch (error) {
+        console.error(error);
+        throw new createHttpError.InternalServerError(error);
+    }
+
     return {
         statusCode: 200,
-        body: JSON.stringify({})
+        body: JSON.stringify(updatedAuction)
     }
 };
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
