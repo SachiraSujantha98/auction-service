01a01611c955f7f84eb13940da49f4e6518a018d
SachiraSujantha98 <sachirasujanthamp@gmail.com>
2025-02-13 15:44:09 +0530

    upload picture validations

--- src/handlers/uploadAuctionPicture.ts
+++ src/handlers/uploadAuctionPicture.ts
@@ -4,19 +4,29 @@ import createHttpError from "http-errors";
 import { uploadPictureToS3 } from "../lib/uploadPictureToS3";
 import { getAuctionById } from "./getAuction";
 import { setAuctionPictureUrl } from "../lib/setAuctionPictureUrl";
+import validator from "@middy/validator";
+import uploadAuctionPictureSchema from "../lib/schemas/uploadAuctionPictureSchema";
 
 const uploadAuctionPicture = async (event) => {
     const { id } = event.pathParameters;
+    const { email } = event.requestContext.authorizer;
     const auction = await getAuctionById(id);
-    const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
-    const buffer = Buffer.from(base64, 'base64');
+
+    // Validate auction ownership
+    if (auction.seller !== email) {
+        throw new createHttpError.Forbidden(
+            "You are not the seller of this auction!"
+        );
+    }
+
+    const base64 = event.body.replace(/^data:image\/\w+;base64,/, "");
+    const buffer = Buffer.from(base64, "base64");
 
     let updatedAuction;
 
     try {
-        const pictureUrl = await uploadPictureToS3(auction.id + '.jpg', buffer);
+        const pictureUrl = await uploadPictureToS3(auction.id + ".jpg", buffer);
         updatedAuction = await setAuctionPictureUrl(auction.id, pictureUrl);
-
     } catch (error) {
         console.error(error);
         throw new createHttpError.InternalServerError(error);
@@ -24,8 +34,10 @@ const uploadAuctionPicture = async (event) => {
 
     return {
         statusCode: 200,
-        body: JSON.stringify(updatedAuction)
-    }
+        body: JSON.stringify(updatedAuction),
+    };
 };
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+    .use(httpErrorHandler())
+    .use(validator({ eventSchema: uploadAuctionPictureSchema }));
